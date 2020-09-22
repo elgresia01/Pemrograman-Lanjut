30246ebb2461a943a96fb92add227c88b2cb9414
elgresia01 <elgresiaegita11@gmail.com>
2020-09-22 16:15:15 +0700

    first commit

---
+++ Tugas 1 Pemrograman Lanjut.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() 
+{
+	printf("\nBiodata \t\t\n");
+	printf("--------\n\n");
+	printf("Nama               : Elgresia Egita Br Perangin-angin\n");
+	printf("NPM                : 19081010086\n");
+	printf("Fakultas/Jurusan   : Ilmu Komputer/ Informatika\n");
+	printf("Tempat/ Tgl Lahir  : Kabanjahe, 01 Juni 2001\n");
+	printf("Alamat             : Jln. Bunga Wijaya Kesuma, Medan\n");
+	printf("Jenis Kelamin      : Perempuan\n");
+	printf("Agama              : Kristen\n");
+	printf("No Telepon         : 085836461118\n");
+		
+	return 0;
+}
